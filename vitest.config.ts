49316863b821e4d9d0e1a888656cee95997607be
iggusti <iggusti@gmail.com>
2026-01-21 15:48:58 +0700

    fix: tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    pool: "threads",
+    pool: "forks",
   },
   resolve: {
     alias: {
